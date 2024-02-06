9267c162533b77ce1a54f3ed0caa06f2b3854781
BestBearr <32557819+BestBearr@users.noreply.github.com>
2024-02-06 05:54:44 -0500

    refactor: fix incorrect indent

--- src/main/java/co/crystaldev/factions/command/VersionCommand.java
+++ src/main/java/co/crystaldev/factions/command/VersionCommand.java
@@ -27,10 +27,10 @@ public final class VersionCommand extends BaseFactionsCommand {
         String authors = String.join(", ", AlpineFactions.getInstance().getDescription().getAuthors());
         Messaging.send(sender, Reference.MINI_MESSAGE.deserialize(String.join("<br>",
                 "<info>.</info>",
-                        "<info>|</info> This server is running <b><gradient:#00aaaa:#78cccc:#00aaaa>AlpineFactions</gradient></b>",
-                        "<info>|</info> <emphasis>Version:</emphasis> <color:#b8f2f2>v" + Reference.VERSION,
-                        "<info>|</info> <emphasis>Authors:</emphasis> <color:#b8f2f2>" + authors,
-                        "<info>'</info>"
+                "<info>|</info> This server is running <b><gradient:#00aaaa:#78cccc:#00aaaa>AlpineFactions</gradient></b>",
+                "<info>|</info> <emphasis>Version:</emphasis> <color:#b8f2f2>v" + Reference.VERSION,
+                "<info>|</info> <emphasis>Authors:</emphasis> <color:#b8f2f2>" + authors,
+                "<info>'</info>"
         )));
     }
 }
