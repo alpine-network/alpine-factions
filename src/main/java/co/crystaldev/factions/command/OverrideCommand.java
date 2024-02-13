932bfb0fde37321162df734070c467ceeb1fbcb0
BestBearr <32557819+BestBearr@users.noreply.github.com>
2024-02-12 21:49:45 -0500

    add: override command

---
+++ src/main/java/co/crystaldev/factions/command/OverrideCommand.java
@@ -0,0 +1,35 @@
+package co.crystaldev.factions.command;
+
+import co.crystaldev.alpinecore.AlpinePlugin;
+import co.crystaldev.factions.PermissionNodes;
+import co.crystaldev.factions.command.framework.FactionsCommand;
+import co.crystaldev.factions.config.MessageConfig;
+import co.crystaldev.factions.config.type.ConfigText;
+import co.crystaldev.factions.handler.PlayerHandler;
+import co.crystaldev.factions.handler.player.PlayerState;
+import dev.rollczi.litecommands.annotations.command.Command;
+import dev.rollczi.litecommands.annotations.context.Context;
+import dev.rollczi.litecommands.annotations.description.Description;
+import dev.rollczi.litecommands.annotations.execute.Execute;
+import dev.rollczi.litecommands.annotations.permission.Permission;
+import org.bukkit.entity.Player;
+
+/**
+ * @author BestBearr <crumbygames12@gmail.com>
+ * @since 02/12/2024
+ */
+@Command(name = "factions override")
+@Description("Override factions permission checks.")
+@Permission(PermissionNodes.ADMIN)
+public final class OverrideCommand extends FactionsCommand {
+    public OverrideCommand(AlpinePlugin plugin) {
+        super(plugin);
+    }
+
+    @Execute
+    public void execute(@Context Player player) {
+        PlayerState state = PlayerHandler.getInstance().getPlayer(player);
+        state.setOverriding(!state.isOverriding());
+        MessageConfig.getInstance().stateChange.send(player, "subject", "Override Mode", "state", state.isOverriding());
+    }
+}
