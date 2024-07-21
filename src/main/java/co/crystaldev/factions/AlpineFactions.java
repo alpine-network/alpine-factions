a8812579194de31b0336f8d7d12219c7370b4be3
BestBearr <32557819+BestBearr@users.noreply.github.com>
2024-07-21 02:34:31 -0400

    Fixed: conform to AlpineCore refactor

--- src/main/java/co/crystaldev/factions/AlpineFactions.java
+++ src/main/java/co/crystaldev/factions/AlpineFactions.java
@@ -1,8 +1,8 @@
 package co.crystaldev.factions;
 
 import co.crystaldev.alpinecore.AlpinePlugin;
-import co.crystaldev.alpinecore.config.AlpineCoreConfig;
 import co.crystaldev.alpinecore.framework.Activatable;
+import co.crystaldev.alpinecore.framework.config.AlpinePluginConfig;
 import co.crystaldev.alpinecore.framework.config.object.ConfigMessage;
 import co.crystaldev.factions.api.Factions;
 import co.crystaldev.factions.api.FlagRegistry;
@@ -77,13 +77,13 @@ public final class AlpineFactions extends AlpinePlugin implements Factions {
     }
 
     @Override
-    public void setupDefaultVariables(@NotNull VariableConsumer variableConsumer) {
+    public void setupVariables(@NotNull VariableConsumer variableConsumer) {
         variableConsumer.addVariable("prefix", "<info>Factions</info> <separator>»</separator><text>");
         variableConsumer.addVariable("error_prefix", "<error>Factions</error> <separator>»</separator><text>");
     }
 
     @Override
-    public void setupDefaultConfiguration(@NotNull AlpineCoreConfig config) {
+    public void setupAlpineConfig(@NotNull AlpinePluginConfig config) {
         config.noPages = ConfigMessage.of("%error_prefix% No pages available to display");
     }
 
