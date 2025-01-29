b297e5900aaf5058d8df6c315cdd95be0d6015f2
lilyisnotok <92556343+slidesL@users.noreply.github.com>
2025-01-28 23:47:00 -0500

    change: remove null yaw/pitch check (desc)
    This change will break with warps set before version 0.2.3

--- src/main/java/co/crystaldev/factions/util/adapter/LocationTypeAdapter.java
+++ src/main/java/co/crystaldev/factions/util/adapter/LocationTypeAdapter.java
@@ -52,12 +52,8 @@ public final class LocationTypeAdapter extends TypeAdapter<Location> {
         double x = obj.get("x").getAsDouble();
         double y = obj.get("y").getAsDouble();
         double z = obj.get("z").getAsDouble();
-        // TODO: temporary, as before 0.2.3 all yaw & pitch values are null/don't exist
-        // TODO: CHANGE BEFORE MAP 2
-//        float yaw = obj.get("yaw").getAsFloat();
-//        float pitch = obj.get("pitch").getAsFloat();
-        float yaw = obj.has("yaw") && !obj.get("yaw").isJsonNull() ? obj.get("yaw").getAsFloat() : 0.0F;
-        float pitch = obj.has("pitch") && !obj.get("pitch").isJsonNull() ? obj.get("pitch").getAsFloat() : 0.0F;
+        float yaw = obj.get("yaw").getAsFloat();
+        float pitch = obj.get("pitch").getAsFloat();
 
         return new Location(worldName == null ? null : Bukkit.getWorld(worldName), x, y, z, yaw, pitch);
     }
