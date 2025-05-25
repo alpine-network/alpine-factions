c4e2518d1db19893a0b3ede06a3d91f7ffa0fb5f
Thomas Wearmouth <tomwmth@pm.me>
2025-05-26 02:12:38 +0800

    Rewrite buildscripts

--- simple/build.gradle.kts
+++ simple/build.gradle.kts
@@ -1,9 +1,39 @@
 plugins {
-    id("java")
+    java
     alias(libs.plugins.shadow)
+    alias(libs.plugins.blossom)
 }
 
+val pluginName: String by rootProject.properties
+
 dependencies {
-    implementation(project(":"))
-    "shadow"(project(":"))
+    implementation(rootProject)
+    compileOnly(libs.bukkit)
+}
+
+sourceSets {
+    main {
+        blossom {
+            resources {
+                property("version", project.version.toString())
+            }
+        }
+    }
+}
+
+tasks.shadowJar {
+    archiveBaseName.set("Simple${pluginName}")
+    archiveClassifier.set("")
+
+    dependencies {
+        include(dependency(rootProject))
+    }
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.jar {
+    enabled = false
 }
